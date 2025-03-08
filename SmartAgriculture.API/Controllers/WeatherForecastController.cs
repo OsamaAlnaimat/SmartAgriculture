ad4959f6f058fdc907104d438edef9edfec7a71f
osama alnaimat <ofalnaimat21@cit.just.edu.jo>
2025-03-08 18:26:17 +0300

    ...

--- SmartAgriculture.API/Controllers/WeatherForecastController.cs
+++ SmartAgriculture.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SmartAgriculture.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","NewChange"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
