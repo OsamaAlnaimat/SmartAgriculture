83361149bc4c615c92b1e3e380b47b2768f7f2fa
osama alnaimat <ofalnaimat21@cit.just.edu.jo>
2025-03-10 01:35:44 +0300

    Add Farm

---
+++ SmartAgriculture.Domain/Entities/Farm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartAgriculture.Domain.Entities
+{
+    internal class Farm
+    {
+    }
+}
